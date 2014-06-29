fdc4ead809a65cc6ca3512345f8e5cd030feb368
Yaniv Savir <saviry@gmail.com>
2014-06-29 16:54:02 -0400

    fixed registration bug

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -5,14 +5,11 @@ class Students::RegistrationsController < Devise::RegistrationsController
     params[:diets].each do |checked_diet|
       student_diets << StudentDiet.create(:diet_id => Diet.find_by_name(checked_diet[0]).id)
     end
-    super
     student_diets.each do |student_diet|
       student_diet.student = current_student
       student_diet.save!
     end
-    current_student.school = School.find(params["school_id"])
-    current_student.save!
-    redirect_to root_path
+    super
   end
 
 end
