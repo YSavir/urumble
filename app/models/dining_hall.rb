0de80f098df76dbd57092ed606f0b83dca94b9cd
Yaniv Savir <saviry@gmail.com>
2014-06-29 13:19:37 -0400

    work on showing reviews and dining halls

--- app/models/dining_hall.rb
+++ app/models/dining_hall.rb
@@ -4,4 +4,13 @@ class DiningHall < ActiveRecord::Base
   has_many :reviews
   has_many :photos
 
+  def average_rating
+    rating_total = 0
+    self.reviews.each do |review|
+      rating_total += review.rating if review.rating
+    end
+    average = rating_total.to_f / self.reviews.length
+    average.round(2)
+  end
+
 end
