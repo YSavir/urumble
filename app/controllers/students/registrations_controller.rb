c0e87cf9a02b1bcc9bfdfaab866539225fcbd61c
Yaniv Savir <saviry@gmail.com>
2014-06-29 15:48:48 -0400

    registration fully implemented

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,9 +1,17 @@
 class Students::RegistrationsController < Devise::RegistrationsController
 
   def create
-
-
+    student_diets = []
+    params[:diets].each do |checked_diet|
+      student_diets << StudentDiet.create(:diet_id => Diet.find_by_name(checked_diet[0]).id)
+    end
     super
+    student_diets.each do |student_diet|
+      student_diet.student = current_student
+      student_diet.save!
+    end
+    current_student.school = School.find(params["school_id"])
+    current_student.save!
   end
 
 end
